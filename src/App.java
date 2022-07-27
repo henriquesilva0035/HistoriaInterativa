ab80f4578e3eae46a0991b1f7697bbdcde87e5c2
henriquesilva0035 <henrique.jorge@ufrpe.br>
2022-07-27 00:02:40 -0300

    Classe
    criação da classe personagem

--- src/App.java
+++ src/App.java
@@ -5,8 +5,12 @@ public class App {
     static Scanner escaneador;
 
     public static void main(String[] args) {
-        System.out.println("Olá, seja bem vindo ao Chapeu Seletor");
-        System.out.println("\n Voce está preparado para descobrir a sua casa?");
+
+        Personagem neon = new Personagem("Neon", 100);
+        Personagem sova = new Personagem("Sova", 100);
+
+        System.out.println("Olá, seja bem vindo ao Medieval");
+        System.out.println("\n Voce está preparado para descobrir os segredos desse lugar?");
         System.out.println("\n\n Antes de Começar, como você se chama?");
         // inicio do sistema onde vai ter perguntas para o user
         escaneador = new Scanner(System.in);
@@ -16,47 +20,63 @@ public class App {
         // vou usar ela a seguir.
 
         System.out.println("\n\n Humm " + nomejogadordigitado + " Seja bem vindo");
+        // 1 cap
+        System.out.println(
+                "\n A região de Runiterra é conhecida como um lugar bastante violento e perigoso, sobreviver la ja é dificil para os moradores,");
+
+        System.out.println("Agora imagina para dois jovens curiosos.");
 
         System.out.println(
-                "\n Lar dos bravos e cavalheiros, A Casa valoriza feitos de coragem e superação pessoal que costumam ser realizados por altruísmo");
-        System.out.println(
-                "\n Seus estudantes tendem a ser bastante aventureiros, mas se destacam, sobretudo, pela lealdade.");
+                "\n Sova e Neon Sao amigos a muitos anos, e em um final de tarde resolveram andar sem rumo apenas para conversar sobre o dia.");
 
-        System.out.println("\n Qual dessas principais caracteristicas você considera ter");
         System.out.println(
-                "\n\n Opção 1: uma pessoa imprudente nos atos de bravura, bastante aventureiro, \n\n Opção 2: Voce considera ser uma pessoa honesta e paciente ");
+                "os mesmo acabam perdendo noção do tempo e lugar e acabam bem longe de casa, ao perceberem ficam assustados e preocupados");
+        System.out.print(
+                "Sova encontra uma trilha indo para a esquerda e logo mostra a Neon, Sova entao pergunta se a Neon topa seguir a trilha.");
+
+        System.out.println("\n  o que voce faz? ");
+        System.out.println(
+                "\n\n Opção 1: Seguir a trilha com o Sova, \n\n Opção 2: Continuar seguindo em frente ");
 
         escaneador = new Scanner(System.in);
         int escolha = escaneador.nextInt();
 
         if (escolha == 1) {
 
-            System.out.println("Humm vejo que a casa que mais se encaixa com você é Grifinória!");
+            System.out.println(
+                    neon.nome + "  e  " + sova.nome
+                            + "Neon seguem a trilha, depois de andar por 5 minutos encontram uma caverna e ao entrar encontram uma raposa que avança em direção do Sova");
+            sova.alterarEnergia(-200);
             // essa if 1 e ja é o final 1
 
         } else if (escolha == 2) {
-            System.out.println("Bom ainda nao consigo te encaixar em uma casa, vamos tentar mais uma vez");
+            System.out.println(
+                    "Neon fala com o Sova para tentarem seguir o caminho que vinheram para assim chegar em casa");
 
             // daqui pra baixo começa a 2 parte junto com os 2 finais
             System.out.println(
-                    "\n são dedicados, pacientes e honestos. Suas admiráveis características os tornam trabalhadores árduos e modestos, sendo receptivos a todos os demais tipos de bruxos.");
-            System.out.println("\n por outro lado, o excesso dessas qualidades pode se tornar um ponto negativo");
-
-            System.out.println("\n Qual dessas principais caracteristicas você considera ter");
+                    "\n Sova e Neon continuam seguindo o caminho que fizeram com fé em encontrar o caminho certo para casa");
+            System.out.println(
+                    "\n depois de percorrer um longo caminho notaram que a paissagem parecia nao mudar, ao fundo Sova da um grito por Neon");
+            System.out.print("\n Neon! Aqui acho que encontrei o caminho certo");
+            System.out.println("\n Neon vai ate o Sova?");
 
             System.out.println(
-                    "\n\n Opção 1: Pessoa honesta, paciente e dedicada, \n\n Opção 2: Uma pessoa ambiciosa, um senso de competição muito aflorado ");
+                    "\n\n Opção 1: Sim, acho que ele encontrou o caminho, \n\n Opção 2: vou fingir que nao vi ");
 
             escaneador = new Scanner(System.in);
             int escolha1 = escaneador.nextInt();
 
             if (escolha1 == 1) {
 
-                System.out.println("Lufa-Lufa é a sua casa!");
+                System.out.println(
+                        "Neon se aproxima de Sova e ver as casas, sinal que estavam proximo de casa, Neon fica muito feliz com o Sova");
+                sova.alterarEnergia(+20);
                 // essa if
 
             } else if (escolha1 == 2) {
-                System.out.println("Sonserina é a sua casa!");
+                System.out.println("Sova fica irritado por Neon nao lhe ouvir e pensa em deixar ela ");
+                sova.alterarEnergia(-10);
             }
 
         } // chave final do else if
---
+++ src/Personagem.java
@@ -0,0 +1,31 @@
+public class Personagem {
+
+    String nome;
+    int energia;
+
+    Personagem(String nome, int energia) {
+        this.nome = nome;
+        this.energia = 100;
+
+    }
+
+    void alterarEnergia(int alteracao) {
+        this.energia = this.energia + alteracao;
+
+        if (alteracao > 0) {
+            System.out.println(
+                    "Como decorrência dessa ação," + this.nome + "Recupera" + alteracao + " pontos de energia");
+        } else {
+            System.out
+                    .println("Como decorrência dessa ação," + this.nome + "Perdeu" + alteracao + " pontos de energia");
+        }
+
+        if (this.energia > 100) {
+            this.energia = 100;
+        } else if (this.energia < 0) {
+            this.energia = 0;
+            System.out.println("Sua energia esgotou" + this.nome + " foi ferido não consegue  seguir viagem ");
+        }
+    }
+
+}
